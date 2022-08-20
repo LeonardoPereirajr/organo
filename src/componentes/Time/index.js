79d3971de3c508c55e0a9deed2ef3bce557968e4
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2022-08-20 15:17:24 -0300

    Criação de squads

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,14 @@
+import './Time.css'
+
+const Time = (props) => {
+    const css = {backgroundColor:props.corSecundaria}
+    const border = {borderColor: props.corPrimaria}
+    return (
+        <section className='time' style={css}>
+            <h3 style={border}>{props.nome}</h3>
+        </section>
+       )
+
+}
+
+export default Time
