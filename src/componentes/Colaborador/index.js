d37efd5a189e8bfcdc27dd76b25750bc62199031
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2022-08-20 15:36:08 -0300

    Inserindo cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return <div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/LeonardoPereirajr.png' alt='Leonardo Pereira'/>
+        </div>
+        <div className='rodape'>
+            <h4>Leonardo Pereira</h4>
+            <h5>Desenvolvedor</h5>
+        </div>
+    </div>
+}
+
+export default Colaborador
